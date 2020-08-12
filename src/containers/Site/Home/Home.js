bae4bc7e74cd053d10ce206087a66a9d0e87148e
Mo Salah <mohumed.s.omar@gmail.com>
2020-08-12 09:07:45 +0200

    edit design

--- src/containers/Site/Home/Home.js
+++ src/containers/Site/Home/Home.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import moment from "moment";
+import axiosBaseAuth from "../../../shared/axiosBaseAuth";
+import "./Home.css";
 
 export default function Home() {
+  const [allScores, setAllScores] = useState([]);
+  useEffect(() => {
+    axiosBaseAuth.get("/api/user/self-user-score").then((res) => {
+      setAllScores(res.data);
+      console.log(res.data);
+    });
+  }, []);
   return (
-    <div>
-      <Link to="/site/take-quiz"> Enroll Qutions </Link>
-      <h1>Last Scores</h1>
+    <div className="home-container">
+      <div>
+        <Link to="/site/take-quiz"> Enroll Qutions </Link>
+        <h1>Last Scores</h1>
+        {allScores.map((el) => (
+          <div className="score-item">
+            <span>Score : {el.score}</span>
+            <span>Take At : {moment(el.createdAt).format("DD/MM/YYYY")}</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
